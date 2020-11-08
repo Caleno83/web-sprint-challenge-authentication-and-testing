d2987edd037c5167ba04c608211f0aa47831c9fb
jorge jimenez <cleno@jorges-MacBook-Air.local>
2020-11-08 12:04:05 -0500

    Done with authentication flow with creation of login, and resgister, and middleware use

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
-const authenticate = require('../auth/authenticate-middleware.js');
+// const authentication = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
 
@@ -13,6 +13,17 @@ server.use(cors());
 server.use(express.json());
 
 server.use('/api/auth', authRouter);
-server.use('/api/jokes', authenticate, jokesRouter);
+server.use('/api/jokes', jokesRouter);
+
+server.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).json({
+      message: "Something went wrong, please try again later",
+    });
+  });
+
+server.get("/", (req, res) => {
+    res.send(`<h2>Welcome To My Module 3 Spring Challenge</h2>`);
+});
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,75 @@
-const router = require('express').Router();
+const express = require('express')
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const db = require("../users/usersModel")
 
-router.post('/register', (req, res) => {
+
+const router = express.Router()
+
+router.post('/register', async (req, res, next) => {
   // implement registration
+  try {
+		const { username, password } = req.body
+		const user = await db.findBy({username}).first()
+		// const user = await db.findByUsername(username)
+
+
+		if (user) {
+			return res.status(409).json({
+				message: "You Shall Not Pass",
+			})
+		}
+
+		const newUser = await db.add({
+			username,
+      password: await bcrypt.hash(password, 14),
+		})
+
+		.then(info => {res.status(201).json({message: "Registration Successful", info});})
+
+		res.status(201).json(newUser)
+	} catch(err) {
+		next(err)
+	}
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res, next) => {
   // implement login
+  try {
+		const { username, password } = req.body
+		const user = await db.findBy({ username }).first()
+		// const user = await db.findByUsername(username)
+		
+		if (!user) {
+			return res.status(401).json({
+				message: "You Shall Not Pass",
+			})
+		}
+
+		const passwordValid = await bcrypt.compare(password, user.password)
+
+		if (!passwordValid) {
+			return res.status(401).json({
+				message: "You Shall Not Pass",
+			})
+		}
+
+		const token = jwt.sign({
+			userID: user.id,
+		}, process.env.JWT_SECRET)
+
+
+    
+        
+		res.json({
+			message: `Welcome ${user.username}!`,
+			token,
+			user_id: user.id,
+
+		})
+	} catch(err) {
+		next(err)
+	}
 });
 
 module.exports = router;
